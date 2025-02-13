61a0ff0c7d2ba963631b1ee011f12021afcfc20f
abdulrahman <abdulrahman403868@fci.bu.edu.eg>
2025-02-13 14:40:53 +0200

    Add project files.

---
+++ Assignment02-EF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment02_EF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
